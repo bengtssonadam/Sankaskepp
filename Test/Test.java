344fb06768b6017bd9380f7244c5d0df5775b791
Adam Bengtsson <adambengtsson@MacBook-Pro-som-tillhor-Adam.local>
2021-11-02 00:07:58 +0100

    created branch test so I can test my code

---
+++ Test/Test.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Main {
+    @Test
+
+}
+/* TDD Test bestäm försättningarna så att mitt test kan visa vad som händer eller vilket tillstånd mitt program befinner sig i.
+
+Sänka skepp:
+
+Vad händer om motståndare inte har några skepp
+Utanför kartan
+Miss
+Träff
+Vunnit
+Redan träffat
+Skrivit fel koordinater
+*/
