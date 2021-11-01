b6596de43af1b4e9d6d40d3cd28124757707dced
Adam Bengtsson <adambengtsson@MacBook-Pro-som-tillhor-Adam.local>
2021-11-01 12:42:37 +0100

    created computer turn and quitgame and print gamemap.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static int playerShips;
     public static int computerShips;
     public static String[][] gameBoard = new String[numRows][numCols];
-    public static int[][] missedGuesses = new int[numRows][numCols];
+    //public static int[][] missedGuesses = new int[numRows][numCols];
     private static void createGameBoard() {
         //Y-coordinats 1-9 under gameboard
         System.out.print("  ");
@@ -36,7 +36,7 @@ public class Main {
     private static void placePlayersShips() {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("\nPlayer 1: Place your ships:");
+        System.out.println("\nWelcomem Captain : Place your ships:");
         //Place 3 ships for player
         playerShips = 3;
         for (int i = 1; i <= playerShips;) {
@@ -49,7 +49,8 @@ public class Main {
                 gameBoard[x][y] = "ps";
                 i++;
             } else if ((x > 0 && x <= numRows) && (y < 0  && y >= numCols && gameBoard[x][y] == "ps")){
-                System.out.println("You alredy have a ships on this coordinats");  }
+                System.out.println("You alredy have a ships on this coordinats");
+            }
             else if ((x < 0 || x > numRows) || (y < 0 || y > numCols))
                 System.out.println("You can't place your ship outside the " + numRows + " by " + numCols + " gameboard");
             }
@@ -98,7 +99,7 @@ public class Main {
                 System.out.print("Set col coordinat for strik");
                 y = input.nextInt();
                 // Check so coordinats is valid
-                if ((x < 0 && x <= numRows) && (y < 0 && y <= numCols)) {
+                if ((x > 0 && x <= numRows) && (y > 0 && y <= numCols)) {
                 //Check for computer ship
                    if (gameBoard[x][y] == "cs") {
                        System.out.println(" Good shoot Captain, thats a HIT!!!");
@@ -123,8 +124,71 @@ public class Main {
 
     private static void computerTurn() {
         System.out.println(" Now its computer's turn to shot, Watch out so they don't hit one of your ship.");
-    }
+        int x = 0;
+        int y = 0;
 
+        //random pick x & y coordinats for computers shots.
+        do {
+            x = (int) (Math.random()*10);
+            y = (int) (Math.random()*10);
+
+            if ((x > 0 && x <= numRows) && (y > 0 && y <= numCols))
+            {
+                if (gameBoard[x][y] == "ps")  {
+                    System.out.println("The Computer sunk one of your ships Captain !!!");
+                    gameBoard[x][y] = "oo";
+                    playerShips--;
+                }
+
+                else if (gameBoard[x][y] == "cs") {
+                    System.out.println("Captain don't worry!!! The Computer is sinking there own ship.");
+                    gameBoard[x][y] = "xx";
+                    computerShips--;
+                }
+
+                else if (gameBoard[x][y] == "--") {
+                    System.out.println("Captain don't worry!!! The Computer is hitting the water!!");
+                }
+        }
+        }
+                while ((x < 0 || x >= numRows) || (y < 0 || y >= numCols)) ;
+
+        }
+
+        public static void gameOver(){
+                System.out.println("Your ships: " + playerShips + " | Computer ships: " + computerShips);
+                if(playerShips > 0 && computerShips <= 0)
+                    System.out.println("good figth Captain!!! You won the battle");
+                else
+                    System.out.println("Sorry Captain, You lost all your ships");
+                System.out.println();
+            }
+    private static void printOceanMap() {
+        System.out.println();
+        //Y-coordinats 1-9 over gameboard
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i +1 +" ");
+        System.out.println();
+
+        //X-coordinats on the sides of gameboard
+        for(int x = 0; x < gameBoard.length; x++) {
+            System.out.print((x+1) + "|");
+
+            for (int y = 0; y < gameBoard[x].length; y++){
+                System.out.print(gameBoard[x][y]);
+            }
+
+            System.out.println("|" + (1+x));
+        }
+
+        //Y-coordinats 1-9 under gameboard
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i + 1 + " ");
+        System.out.println();
+
+    }
 
 
         public static void main(String[] args) {
@@ -137,7 +201,6 @@ public class Main {
                 GameLoop();
             }
             while(playerShips != 0 && computerShips != 0);
-
             gameOver();
     }
 }
